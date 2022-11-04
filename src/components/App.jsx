23102f6b488ddb2a9776701313774d43b55394ed
unknown <kononov8214@gmail.com>
2022-11-04 23:39:10 +0300

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { InputContact } from "./InputContact";
+
+// export const App = () => {
+ export  class App extends React.Component { 
+  state = {
+    contacts: [],
+    name: ''
+    }
+    render() {
+      return (
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101'
+          }}
+        >
+          <InputContact />
+        </div>
+      );
+    }
+}
+// };
---
+++ src/components/InputContact.jsx
@@ -0,0 +1,17 @@
+
+
+export const InputContact = () => {
+    return(
+    <form >
+        <h1>Name</h1>
+            <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                />
+                <button>Add contact </button>
+    </form>
+                )
+}
